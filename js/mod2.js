7a66fb84598334de096312b40a4a33adf80cf4e2
EricEisaman <eeisaman@auburnschl.edu>
2016-11-23 16:10:45 -0500

    Update mod2.js

--- js/mod2.js
+++ js/mod2.js
@@ -1,1 +1,11 @@
-//
+var inpCourse = document.getElementById('course');
+var inpPassword = document.getElementById('sn');
+var btnSubmit = document.getElementById('submit');
+
+inpCourse.addEventListener('change', function(e){
+  alert(inpCourse.value);
+});
+
+btnSubmit.addEventListener('click', function(e){
+  alert(inpPassword.value);
+});
