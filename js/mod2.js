888ce01318b889bdf02bd58188979b2882cd4c29
EricEisaman <eeisaman@auburnschl.edu>
2016-11-23 16:49:24 -0500

    Update mod2.js

--- js/mod2.js
+++ js/mod2.js
@@ -5,10 +5,12 @@ var btnSubmit = document.getElementById('submit');
 inpCourse.addEventListener('change', function(e){
   var elem = document.getElementById(inpCourse.value);
   var divs = document.getElementsByTagName('div');
-  divs.forEach(function(d){
-    d.className += 'hide';
-  });
-  elem.classList.toggle('show');
+  for(i=0; i<divs.length ; i++){
+    if (divs[i].style.display !== 'none') {
+        divs[i].style.display = 'none';
+    }
+  }
+  elem.style.display = 'block';
 });
 
 btnSubmit.addEventListener('click', function(e){
