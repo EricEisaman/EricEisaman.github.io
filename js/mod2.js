d869f3db11b9422aacdd4af191e3d89ab4bd5550
EricEisaman <eeisaman@auburnschl.edu>
2016-11-24 06:19:41 -0500

    Update mod2.js

--- js/mod2.js
+++ js/mod2.js
@@ -19,7 +19,6 @@ btnSubmit.addEventListener('click', function(e){
 
 var pts = document.getElementsByClassName('pt');
 for(i=0;i<pts.length;i++){
-  /*
   pts[i].addEventListener('click',function(e){
     if(e.target.style.backgroundColor=='pink'){
        e.target.style.backgroundColor='green'
@@ -29,14 +28,4 @@ for(i=0;i<pts.length;i++){
       e.target.style.color='black'
     }
   });
-  */
-  pts[i].onclick = function(e){
-    if(e.target.style.backgroundColor=='pink'){
-       e.target.style.backgroundColor='green'
-       e.target.style.color='white'
-    }else{
-      e.target.style.backgroundColor='pink'
-      e.target.style.color='black'
-    }
-  };
 }
