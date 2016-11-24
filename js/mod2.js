953d6b35b5d04f58ab79d3b42b38dd91ee7acaf8
EricEisaman <eeisaman@auburnschl.edu>
2016-11-24 06:50:14 -0500

    Update mod2.js

--- js/mod2.js
+++ js/mod2.js
@@ -21,7 +21,7 @@ var pts = document.getElementsByClassName('pt');
 console.log('pts : ' , pts);
 for(i=0;i<pts.length;i++){
   pts[i].addEventListener('click',function(e){
-    console.log(e.target);
+    console.log(e.target.style.backgroundColor);
     if(e.target.style.backgroundColor=='pink'){
        e.target.style.backgroundColor='green'
        e.target.style.color='white'
