8ece8d80431d0afe8103f184772cb356cadde836
EricEisaman <eeisaman@auburnschl.edu>
2017-04-10 13:41:15 -0400

    Create canvas.js

---
+++ code/canvas.js
@@ -0,0 +1,9 @@
+c = document.createElement('canvas');
+c.width = 800;
+c.height = 600;
+document.appendChild(c);
+document.body.appendChild(c);
+ctx = c.getContext('2d');
+ctx.beginPath();
+ctx.fillRect(0,0,800,600);
+ctx.closePath();
