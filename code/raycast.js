fdf3606b8b935e68774deb7a835e3cb4d67db90b
EricEisaman <eeisaman@auburnschl.edu>
2017-04-20 17:59:22 -0400

    Create raycast.js

---
+++ code/raycast.js
@@ -0,0 +1,31 @@
+var map_1 = [ 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 ,
+            0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 ,
+            0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 ,
+            0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 ,
+            0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 ,
+            0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 ,
+            0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 ,
+            0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 ,
+            0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0   ];
+
+class Scene{
+  constructor(map){
+    this.map = map;
+    this.player = new Player();
+  }
+  
+}
+
+class vec2{
+  constructor(x,y){
+    this.x = x;
+    this.y = y;
+  }
+}
+
+class Player{
+  constructor(){
+  
+  }
+ 
+}
