9b7a7397784c111cc8094fd27c827753b8286c9e
EricEisaman <eeisaman@auburnschl.edu>
2017-04-20 21:43:26 -0400

    Update raycast.js

--- code/raycast.js
+++ code/raycast.js
@@ -187,11 +187,11 @@ class Graphics{
   }
   start(){
     this._lastTime = new Date().getTime();
-    requestAnimationFrame(this.update()).bind(this);
+    requestAnimationFrame(update);
   }
   update(){
     this._ctx.fillRect(0,0,this._canvas.width,this._canvas.height);
-    requestAnimationFrame(this.update()).bind(this);
+    requestAnimationFrame(update);
   }
 }
 
