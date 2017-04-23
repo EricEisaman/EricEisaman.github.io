99a86eba9604fa4b06e065aa1d46742bbcd680a4
EricEisaman <eeisaman@auburnschl.edu>
2017-04-22 21:02:00 -0400

    Update raycast.js

--- code/raycast.js
+++ code/raycast.js
@@ -1,3 +1,22 @@
+/*
+    GNU General Public License
+    Author: Eric Eisaman  Created: April 22, 2017
+    =============================================
+    
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 'use strict';
 
 class Level{
@@ -89,7 +108,7 @@ class Player{
     this._playerSprite = opts.playerSprite;
     this._speed = 1;
     this._velocity = new Vec2(0,0);
-    this._angle = 30;
+    this._angle = PLAYERANGLE;
     this._turning = TURNING.NONE;
     this._moving = MOVING.NONE;
     this._fov = 90;
@@ -174,108 +193,147 @@ class Physics{
     //console.log(this._player.pos);
   }
 }
-
+var PLAYERANGLE = -360 + 720*Math.random();
+var DEGBUG_RAY_ANGLE = PLAYERANGLE - 45;
+console.log(`The debug ray angle is ${DEGBUG_RAY_ANGLE}`);
 class Ray{
   constructor(player,map){
     this._player = player;
     this._map = map;
+    this._debugPoints = [ ];
+    this._nextDXis50 = false;
+    this._nextDYis50 = false;
   }
   cast(){
     let angle = this._player.angle-this._player.fov/2;
     let dtheta = this._player.fov/canvas.width;
     let from = this._player.pos.copy();
     let to = from;
-    let s = 0;
     let result = false;
-    while(!result && (s < 450)){
+    let count = 0;
+    while(!result && count<5 && !stop){
       from = this.nextEdgePoint(from,angle);
       result = this.collides(from);
+      this._debugPoints.push(from);
+      count++;
     }
+    stop = true;
+    console.log(this._debugPoints);
     return result;
   }
   nextEdgePoint(from,angle){
-    let ca = Math.cos(angle);
-    let sa = Math.sin(angle);
+    let ca = Math.cos(angle*Math.PI/180);
+    let sa = Math.sin(angle*Math.PI/180);
     let caPos = (ca>0);
     let saPos = (sa>0);
-    let cLarger = (Math.abs(ca)>Math.abs(sa));
     let dx,dy;
     if(caPos && saPos){
     //top and right
-      dx = 50 - from.x%50;
-      dy = from.y%50;
+    //console.log("top and right");
+      dx = this._nextDXis50 ? 50:50 - from.x%50;
+      dy = this._nextDYis50 ? 50:from.y%50;
+      this._nextDXis50=false;
+      this._nextDYis50=false;
+      //console.log(`dx: ${dx}  dy: ${dy}`);
       if((sa/ca)<(dy/dx)){
         //right
+        //console.log("right");
         let x = Math.round(from.x + dx);
-        let y = Math.round(from.y + dx*Math.tan(angle*Math.PI/180));
+        let y = Math.round(from.y - dx*Math.tan(angle*Math.PI/180));
         return new Vec2(x,y);
       }else{
         //top
+        //console.log("top");
         let x = Math.round(from.x + dy/Math.tan(angle*Math.PI/180));
         let y = Math.round(from.y - dy);
+        this._nextDYis50=true;
         return new Vec2(x,y);
       }
     }else if( caPos && !saPos){
     //bottom and right
-      dx = 50 - from.x%50;
-      dy = 50 - from.y%50;
+    //console.log("bottom and right");
+      dx = this._nextDXis50 ? 50:50 - from.x%50;
+      dy = this._nextDYis50 ? 50:50 - from.y%50;
+      //console.log(`dx: ${dx}  dy: ${dy}`);
+      this._nextDXis50=false;
+      this._nextDYis50=false;
       if((Math.abs(sa/ca))<(dy/dx)){
         //right
+        //console.log("right");
         let x = Math.round(from.x + dx);
-        let y = Math.round(from.y + dx*Math.tan(angle*Math.PI/180));
+        let y = Math.round(from.y - dx*Math.tan(angle*Math.PI/180));
+        this._nextDXis50=true;
         return new Vec2(x,y);
       }else{
         //bottom
-        let x = Math.round(from.x + dy/Math.tan(angle*Math.PI/180));
+        //console.log("bottom");
+        let x = Math.round(from.x - dy/Math.tan(angle*Math.PI/180));
         let y = Math.round(from.y + dy);
+        this._nextDYis50=true;
         return new Vec2(x,y);
       }
     }else if( !caPos && !saPos){
     //bottom and left
-      dx = from.x%50;
-      dy = 50 - from.y%50;
+    //console.log("bottom and left");
+      dx = this._nextDXis50 ? 50:from.x%50;
+      dy = this._nextDYis50 ? 50:50 - from.y%50;
+      //console.log(`dx: ${dx}  dy: ${dy}`);
+      this._nextDXis50=false;
+      this._nextDYis50=false;
       if((Math.abs(sa/ca))<(dy/dx)){
         //left
+        //console.log("left");
         let x = Math.round(from.x - dx);
         let y = Math.round(from.y + dx*Math.tan(angle*Math.PI/180));
+        this._nextDXis50=true;
         return new Vec2(x,y);
       }else{
         //bottom
-        let x = Math.round(from.x + dy/Math.tan(angle*Math.PI/180));
+        //console.log("bottom");
+        let x = Math.round(from.x - dy/Math.tan(angle*Math.PI/180));
         let y = Math.round(from.y + dy);
         return new Vec2(x,y);
       }
     }else{
     //top and left
-      dx = from.x%50;
-      dy = from.y%50;
+    //console.log("top and left");
+      dx = this._nextDXis50 ? 50:from.x%50;
+      dy = this._nextDYis50 ? 50:from.y%50;
+      //console.log(`dx: ${dx}  dy: ${dy}`);
+      this._nextDXis50=false;
+      this._nextDYis50=false;
       if((Math.abs(sa/ca))<(dy/dx)){
         //left
+        //console.log("left");
         let x = Math.round(from.x - dx);
         let y = Math.round(from.y + dx*Math.tan(angle*Math.PI/180));
+        this._nextDXis50=true;
         return new Vec2(x,y);
       }else{
         //top
+        //console.log("top");
         let x = Math.round(from.x + dy/Math.tan(angle*Math.PI/180));
         let y = Math.round(from.y - dy);
+        this._nextDYis50=true;
         return new Vec2(x,y);
       }
     }
   }
   collides(from){
-    return from;
+    //
+    return false;
   }
 }
 // Each map cell is 50x50 in First Person Viewport
-var map_1 = [ 0 , 0 , 0 , 0 , 1 , 0 , 0 , 0 , 0 , // indices 0-8
-              0 , 0 , 0 , 0 , 1 , 0 , 0 , 0 , 0 , // indices 9-17
-              0 , 0 , 0 , 0 , 1 , 0 , 0 , 0 , 0 , // indices 18-26
-              0 , 0 , 0 , 0 , 1 , 0 , 0 , 0 , 0 , // indices 27-35
-              0 , 0 , 0 , 0 , 1 , 1 , 1 , 1 , 1 , // indices 36-44
-              0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , // indices 45-53
-              0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , // indices 54-62
-              0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , // indices 63-71
-              0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 ];// indices 72-80
+var map_1 = [ 1 , 1 , 1 , 1 , 1 , 0 , 0 , 0 , 0 , // indices 0-8
+                      1 , 0 , 0 , 0 , 1 , 0 , 0 , 0 , 0 , // indices 9-17
+                      1 , 0 , 0 , 0 , 1 , 0 , 0 , 0 , 0 , // indices 18-26
+                      1 , 0 , 0 , 0 , 1 , 0 , 0 , 0 , 0 , // indices 27-35
+                      1 , 0 , 0 , 0 , 1 , 1 , 1 , 1 , 1 , // indices 36-44
+                      1 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 1 , // indices 45-53
+                      1 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 1 , // indices 54-62
+                      1 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 1 , // indices 63-71
+                      1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 ];// indices 72-80
 
 class Graphics{
   constructor(opts){
@@ -291,39 +349,64 @@ class Graphics{
     requestAnimationFrame(this.update.bind(this));
   }
   update(){
+    if(stop){
+      let pts = this._ray._debugPoints;
+      this.drawMinimap({debug:true,pts:pts});
+      return;
+    }
     //First Person Viewport Background
     this._ctx.fillStyle = 'black';
     this._ctx.fillRect(0,0,450,450);
     //First Person Viewport RayCasting
-    for(let i=0; i<canvas.width; i++){
-      let objToRender = this._ray.cast();
-      this.renderVerticalLine(objToRender);
-      this._ray._angle += this._dtheta;
-    }
-    //Minimap Background
+    // for(let i=0; i<canvas.width; i++){
+    //   let objToRender = this._ray.cast();
+    //   this.renderVerticalLine(objToRender);
+    //   this._ray._angle += this._dtheta;
+    // }
+    this._ray.cast();
+    //Minimap
+    this.drawMinimap({debug:false});
+    requestAnimationFrame(this.update.bind(this));
+  }
+  drawMinimap(opt){
+    this.drawMinimapBackground();
+    this.drawMinimapWalls();
+    this.drawMinimapPlayer();
+    if(opt.debug)this.drawMinimapPoints(opt.pts);
+  }
+  drawMinimapBackground(){
     this._ctx.fillStyle = 'white';
-    this._ctx.fillRect(525,0,200,200);
-    //Minimap Walls
+    this._ctx.fillRect(0,0,140,140);
+  }
+  drawMinimapWalls(){
     this._ctx.fillStyle = 'red';
     for( let i=0; i<this._level.map.length; i++){
       if(this._level.map[i]){
-        let x = (i%9)*200/9+525;
-        let y = 200/9*Math.floor(i/9);
+        let x = (i%9)*140/9;
+        let y = 140/9*Math.floor(i/9);
         //console.log("map["+i+"]  x: "+x+"  y: "+y);
-        this._ctx.fillRect(x,y,200/9,200/9);
+        this._ctx.fillRect(x,y,140/9,140/9);
       }
     }
-    //Minimap Player ( Note: we scale down the coordinates for the minimap )
+  }
+  drawMinimapPlayer(){
     this._ctx.fillStyle = 'blue';
-    this._ctx.fillRect(this._player.pos.x * (200/450) - (10 * (200/450)) + 525,
-                       this._player.pos.y * (200/450) - (10 * (200/450)),
-                       20 * (200/450),
-                       20 * (200/450));
-
-    requestAnimationFrame(this.update.bind(this));
+    this._ctx.fillRect(this._player.pos.x * (140/450) - (7 * (140/450)),
+                       this._player.pos.y * (140/450) - (7 * (140/450)),
+                       14 * (140/450),
+                       14 * (140/450));
+  }
+  drawMinimapPoints(pts){
+    for(let i=0; i<pts.length; i++){
+      this._ctx.fillStyle = 'green';
+      this._ctx.fillRect(pts[i].x * (140/450) - (7 * (140/450)),
+                         pts[i].y * (140/450) - (7 * (140/450)),
+                         12 * (140/450),
+                         12 * (140/450));
+    }
   }
   renderVerticalLine(objToRender){
-    console.log(objToRender);
+    //console.log(objToRender);
     if(objToRender){
 
     }
@@ -350,3 +433,6 @@ var myScene = new Scene({
   player: myPlayer,
   canvas: canvas
 });
+
+//FOR DEBUGGING I SET THIS TRUE AT A BREAKPOINT
+var stop = false;
